a3bd71eb5fec09d20dbaf47b7878bbb5f387b9a8
Sébastien Chopin <seb@nuxt.com>
2024-04-26 12:47:51 +0200

    add logs

--- server/api/env.ts
+++ server/api/env.ts
@@ -1,6 +1,8 @@
 export default eventHandler(event => {
   const runtimeConfig = useRuntimeConfig(event)
 
+  console.log('i.meta.env.NUXT_HELLO: ' + import.meta.env.NUXT_HELLO)
+
   return {
     'runtimeConfig().hello': useRuntimeConfig().hello,
     'runtimeConfig(event).hello': runtimeConfig.hello,
